836b8763b33670787b01ecd76035f90077cc8c7f
hamed <h.naimaei@gmail.com>
2023-10-07 15:50:39 +0330

    Update http mini project

--- 17-Http/07-MiniProject/go.mod
+++ 17-Http/07-MiniProject/go.mod
@@ -1,3 +1,3 @@
 module http-mini-project
 
-go 1.18
+go 1.19
--- 17-Http/07-MiniProject/management/server.go
+++ 17-Http/07-MiniProject/management/server.go
@@ -10,7 +10,7 @@ func Run() {
 	mux := http.NewServeMux()
 	mux.Handle("/movies/", &handlers.MovieHandler{})
 	server := &http.Server{
-		Addr:         ":8080",
+		Addr:         ":8085",
 		ReadTimeout:  time.Second * 10,
 		WriteTimeout: time.Second * 10,
 		Handler:      mux,
